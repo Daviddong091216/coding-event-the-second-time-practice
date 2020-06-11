0393b054b85c0e005b63e403dcaa1d3b6c070701
David Dong <daviddong201712@gmail.com>
2020-06-10 21:49:53 -0500

    Before chapter 13 modify build.gradle file to make validation working.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -7,6 +8,7 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @Size()
     private String name;
     private String description;
 
