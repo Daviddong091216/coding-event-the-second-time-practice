314113352675c474573385025d0fd1fa93984d54
David Dong <daviddong201712@gmail.com>
2020-06-10 22:38:46 -0500

    chapter 13.3 is finished. Validating models in a controller.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,8 +4,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,27 +17,33 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title","All Events");
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
     @GetMapping("create")
     public String renderCreateEventForm(Model model) {
-        model.addAttribute("title","Create Event");
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad Data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events";
     }
 
     @GetMapping("delete")
-    public String renderDeleteEventForm(Model model){
-        model.addAttribute("title","Delete Event");
-        model.addAttribute("events",EventData.getAll());
+    public String renderDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Event");
+        model.addAttribute("events", EventData.getAll());
         return "events/delete";
     }
 
